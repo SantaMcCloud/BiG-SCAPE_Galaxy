3c56d410c795b95430d873024cdff55b029eaaee
SantaMcCloud <santino_faack@gmx.de>
2024-01-31 15:34:42 +0100

    add output!

---
+++ mibig_with_input/html_content/js/bigscape_results.js
@@ -0,0 +1,12 @@
+var bigscape_results = [
+    {
+        "label":"2024-01-31_14-15-48_hybrids_glocal_c0.30",
+        "networks":[
+            {
+                "css":"Others",
+                "label":"Others",
+                "name":"Others"
+            }
+        ]
+    }
+];
